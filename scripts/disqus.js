9fa0ab6a345a31866410b2bf015cdcf45bebf23a
massood <massood@email.com>
2017-03-03 15:19:14 +0330

    disqus

---
+++ scripts/disqus.js
@@ -0,0 +1,12 @@
+---
+---
+var disqus_config = function () {
+this.page.url = {{ page.url }};
+this.page.identifier = {{ page.url }};
+};
+(function() {
+var d = document, s = d.createElement('script');
+s.src = 'https://estatic.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
